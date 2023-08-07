dbd75e8c837b3c6b41f5cc3c128c7ccad364fe69
lianggan13 <676021630@qq.com>
2023-08-07 09:45:27 +0800

    s

--- Raspberry/Raspberry.Client/Services/SocketClient.cs
+++ Raspberry/Raspberry.Client/Services/SocketClient.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,76 +10,106 @@ namespace Raspberry.Client.Services
 {
     public class SocketClient
     {
-        private readonly Socket socket;
-        private NetworkStream netStream;
+        private Socket socket;
         public event Action<object, byte[]> Received = null;
         public SocketClient()
         {
-            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
         public void Connect(string serverIp, int serverPort)
         {
             try
             {
-                socket.Connect(serverIp, serverPort);
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                //socket.Connect(serverIp, serverPort);
+                var endPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+                socket.BeginConnect(endPoint, ConnectCallback, null);
             }
-            catch (SocketException ex)
+            catch (Exception ex)
             {
                 Debug.Fail(ex.Message);
             }
+        }
 
-            if (!socket.Connected)
-                return;
-
-            netStream = new NetworkStream(socket, ownsSocket: true);
-            netStream.ReadTimeout = 5000;
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            //socket = (Socket)result.AsyncState;
+            socket.EndConnect(ar);
 
             Task.Factory.StartNew(() =>
                 Receiver(),
                 TaskCreationOptions.LongRunning);
         }
 
+        public void Disconnect()
+        {
+            try
+            {
+                if (socket != null)
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    socket.Dispose();
+                    socket = null;
+
+                    // disconnected event...
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Fail(ex.Message);
+            }
+
+        }
+
         public void Send(string data)
         {
-            if (netStream?.CanWrite == true)
+            //data = $"{data} {DateTime.Now:yyyy MM dd HH:mm:ss}";
+            byte[] buffer = Encoding.UTF8.GetBytes($"{data}");
+            socket.Send(buffer);
+        }
+        const int BufferSize = 1024 * 1024 * 512;
+
+        private void Receiver()
+        {
+            while (socket != null && socket.Connected)
             {
-                //data = $"{data} {DateTime.Now:yyyy MM dd HH:mm:ss}";
-                byte[] buffer = Encoding.UTF8.GetBytes($"{data}");
-                netStream.Write(buffer, 0, buffer.Length);
-                netStream.Flush();
+                try
+                {
+                    if (socket.Poll(int.MaxValue, SelectMode.SelectRead))
+                    {
+                        if (!(socket != null && socket.Connected))
+                            continue;
+
+                        byte[] buffer = new byte[BufferSize];
+                        socket.BeginReceive(buffer, 0, BufferSize, SocketFlags.None, ReceiveAsyncCallback, buffer);
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Disconnect();
+                    Debug.WriteLine(ex.Message);
+                }
             }
         }
 
-        private async void Receiver()
+        private void ReceiveAsyncCallback(IAsyncResult result)
         {
-            const int ReadBufferSize = 1024 * 512;
-            byte[] readBuffer = new byte[ReadBufferSize];
-
-            try
+            int bytesRead = socket.EndReceive(result);
+            if (bytesRead > 0)
             {
-                while (netStream.CanRead)
+                byte[] data = result.AsyncState as byte[];
+                if (bytesRead > 0)
                 {
-                    var data = new List<byte>();
-                    int bytesRead = 0;
-                    while (netStream.DataAvailable)
-                    {
-                        bytesRead = await netStream.ReadAsync(readBuffer, 0, readBuffer.Length);
-                        data.AddRange(readBuffer.Take(bytesRead));
-                        Debug.WriteLine($"Read: {bytesRead}");
-                    }
-
-                    if (bytesRead > 0)
-                        Received?.Invoke(this, data.ToArray());
+                    Debug.WriteLine($"Read: {bytesRead}");
+                    Received?.Invoke(this, data.Take(bytesRead).ToArray());
                 }
-            }
-            catch (OperationCanceledException ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-            finally
-            {
-                Array.Clear(readBuffer, 0, ReadBufferSize);
             }
         }
     }
